025a9e68c3bd7a646b56855b3c396d3cc85a966d
wfbm <william.balan.machado@gmail.com>
2024-05-27 18:49:05 -0300

    4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
-
+    
     loop {
      
         print!("$ ");
@@ -14,6 +15,12 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         let command = input.trim();
-        println!("{command}: command not found")
+        let parsed: Vec<&str> = command.split_whitespace().collect();
+
+        match parsed[..] {
+            ["exit", code] => process::exit(code.parse::<i32>().unwrap()),
+            _ => println!("{command}: command not found"),
+        }
     } 
 }
+
