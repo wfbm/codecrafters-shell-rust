0597511e15dbb0c74e23ef3a2a5b4369f5d54b4a
wfbm <william.balan.machado@gmail.com>
2024-05-27 18:11:44 -0300

    3rd stage

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    loop {
+     
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let command = input.trim();
-    println!("{command}: command not found")
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let command = input.trim();
+        println!("{command}: command not found")
+    } 
 }
